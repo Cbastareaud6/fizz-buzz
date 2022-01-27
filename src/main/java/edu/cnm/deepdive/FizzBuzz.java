e397746d1c047c05f158ae7ffac5a681cdebdba2
Cathy Bastareaud <catybastareaud@gmail.com>
2022-01-27 09:31:29 -0700

    Implement with switch expression

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -17,19 +17,12 @@ public class FizzBuzz {
   }
 
   public String check(int value) {
-    String result;
-    boolean divisibleby3 = value % 3 == 0;
-    boolean divisibleby5 = value % 5 == 0;
-    if (divisibleby3 && divisibleby5) {
-      result = FIZZ_BUZZ;
-    } else if (divisibleby3) {
-      result = FIZZ;
-    } else if (divisibleby5) {
-      result = BUZZ;
-    } else {
-      result = String.valueOf(value);
-    }
-    return result;
+    return switch (value % 15) {
+      case 0 -> FIZZ_BUZZ;
+      case 3, 6, 9, 12 -> FIZZ;
+      case 5, 10 -> BUZZ;
+      default -> String.valueOf(value);
+    };
   }
 
 
